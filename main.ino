0a5ea0f08770c9243d8a77262aa4eb36de88b388
DinoSuarez <31669409+DinoSuarez@users.noreply.github.com>
2021-04-16 09:28:02 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,6 +1,7 @@
 // Eric D. Schubert, 4/16/2021
 // This is a mashup of mostly example code for the Adafruit SSD1306 display and the DHT sensors.
-// Currently, this only displays temperature and humidity on a 128x64 SSD1306 display, but more data will be added
+// Currently, this only displays temperature and humidity on a 128x64 SSD1306 display, but more data will be added.
+// WORK IN PROGRESS, don't expect much.
 
 #include "DHT.h"
 #include <SPI.h>
@@ -12,15 +13,16 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); 
 
-#define DHTPIN 2
+#define DHTPIN 2 
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  dht.begin();
+  
+  dht.begin(); //If you remove this, temperature and humidity are way off
 
    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -31,13 +33,12 @@ void setup() {
   display.display();
   delay(2000); // Pause for 2 seconds
 
-  // Clear the buffer
+  // Clear the buffer and set text types. This is required before printing text
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
 
-  // Draw a single pixel in white
   display.println("Booting...");
 
   // Show the display buffer on the screen. You MUST call display() after
@@ -57,20 +58,19 @@ void loop() {
   display.setCursor(0,0);
 
   // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
   float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
+  // Read temperature as Celsius
   float t = dht.readTemperature();
   // Read temperature as Fahrenheit (isFahrenheit = true)
   float f = dht.readTemperature(true);
 
-  // Check if any reads failed and exit early (to try again).
+  // Check if any reads failed and wait for input
   if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
+    display.println(F("Failed to read from DHT sensor!"));
+    display.display();
     return;
   }
-
-
 
   display.println("Temperature: ");
   display.println(f);
